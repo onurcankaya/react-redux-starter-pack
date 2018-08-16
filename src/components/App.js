cd849ca62e2f08997296a1e758f58c72551b72f4
onurcankaya <onur.eren.cankaya@gmail.com>
2018-08-16 12:50:22 +0200

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <h3>React-Redux Starter Pack</h3>
+      </div>
+    )
+  }
+}
+
+export default App
